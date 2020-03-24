e94a0a0b3e6d1cc36ac48d264c8a16e11ccc68be
Inderjit Gill <email@indy.io>
2020-03-24 13:52:01 +0000

    typo in nodes_edit.sql

--- server/src/sql/notes_edit.sql
+++ server/src/sql/notes_edit.sql
@@ -1,4 +1,4 @@
 UPDATE notes
-SET note_type = $3, source = $4, content = $5, annotation = $6, separator = %7
+SET note_type = $3, source = $4, content = $5, annotation = $6, separator = $7
 WHERE id = $2 and user_id = $1
 RETURNING $table_fields
