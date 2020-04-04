2cc7af34a89f173c9ec9f114a36f9de7ed18cb99
Inderjit Gill <email@indy.io>
2020-04-04 15:10:00 +0100

    removed quote from client side

--- client/src/lib/NoteUtils.js
+++ client/src/lib/NoteUtils.js
@@ -29,21 +29,6 @@ const NoteUtils = {
     return Net.post("/api/notes", data);
   },
 
-  addQuote: (form, extra) => {
-    const notes = splitIntoNotes(form.content.value);
-    if (notes === null) {
-      console.error("NoteUtils.addQuote: splitIntoNotes failed");
-      console.error(form.content.value);
-      return undefined;
-    }
-    let data = { ...extra,
-                 content: notes,
-                 source: form.source.value,
-                 separator: false
-               };
-    return Net.post("/api/quotes", data);
-  },
-
   editNote: (id, data) => {
     const post = {
       id: id,
@@ -54,23 +39,8 @@ const NoteUtils = {
     return Net.put("/api/notes/" + id.toString(), post);
   },
 
-  editQuote: (id, data) => {
-    const post = {
-      id: id,
-      note_type: 2, // a quote
-      separator: false,
-        ...data
-    };
-
-    return Net.put("/api/quotes/" + id.toString(), post);
-  },
-
   deleteNote: (id) => {
     return Net.delete("/api/notes/" + id.toString());
-  },
-
-  deleteQuote: (id) => {
-    return Net.delete("/api/quotes/" + id.toString());
   },
 
   hashByNoteIds: (s) => {
