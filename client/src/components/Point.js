e7d68355c7892c5a82955cff1c643774ff10a3f7
Inderjit Gill <email@indy.io>
2020-04-18 16:56:14 +0100

    better wording on points attached to people and events

--- client/src/components/Point.js
+++ client/src/components/Point.js
@@ -4,12 +4,6 @@ import PointForm from './PointForm';
 export default function Point({ point, parentResource }) {
   const [showForm, setShowForm] = useState(false);
 
-  const date = point.date_textual;
-  const location = point.location_textual;
-  // don't show the label for the main point of an Event (these will always be titled 'Event')
-  //
-  const title = (parentResource === "events" && point.title === "Event") ? "" : `${point.title}:`;
-
   function onShowForm() {
     setShowForm(!showForm);
   }
@@ -24,9 +18,32 @@ export default function Point({ point, parentResource }) {
                        submitMessage="Update Point"></PointForm>);
   }
 
+  let dom;
+  const date = point.date_textual;
+  const location = point.location_textual;
+
+  if (parentResource === "events" && point.title === "Prime") {
+    dom = <p className="subtitle">{ date } { location }</p>;
+  } else {
+    let text;
+    if (location && location.length > 0) {
+      if (point.title === "Born" || point.title === "Died") {
+        text = `${date} ${point.title} in ${location}`;
+      } else {
+        // choosing not to display location, even though one is available
+        // might change this later, or even use location to show a map
+        text = `${date} ${point.title}`;
+      }
+    } else {
+        text = `${date} ${point.title}`;
+    }
+
+    dom = <p className="subtitle">&mdash;{ text }</p>;
+  }
+
   return (
     <div onClick={ onShowForm }>
-      <p className="subtitle">&mdash;{ title } { date } { location }</p>
+      { dom }
       { showForm && buildShowForm () }
     </div>
   );
