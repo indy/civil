c7c4f077b7220417ba3412a89270ae276e5ffcab
Inderjit Gill <email@indy.io>
2020-05-02 15:10:49 +0100

    DeckPoints looks better and link back to their decks

--- client/src/components/DeckPoint.js
+++ client/src/components/DeckPoint.js
@@ -1,13 +1,23 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 export default function DeckPoint({ deckPoint, holderId }) {
-  let className = deckPoint.deck_id === holderId ? 'relevent-deckpoint' : 'deckpoint';
   let pointTitle = deckPoint.point_title === "Prime" && deckPoint.deck_resource === "events" ? "" : deckPoint.point_title;
 
 
-  return (
-    <li className={ className }>
-       { deckPoint.deck_name } &mdash; { pointTitle } { deckPoint.point_date_textual }
-    </li>
-  );
+  let item;
+  if (deckPoint.deck_id === holderId) {
+    item = <li className='relevent-deckpoint'>
+             { deckPoint.deck_name } &mdash; { pointTitle } { deckPoint.point_date_textual }
+           </li>;
+
+  } else {
+    item = <li className='deckpoint'>
+             <Link to={ `/${deckPoint.deck_resource}/${deckPoint.deck_id}` }>
+                { deckPoint.deck_name }  &mdash; { pointTitle } { deckPoint.point_date_textual }
+              </Link>
+           </li>;
+  }
+
+  return item;
 }
