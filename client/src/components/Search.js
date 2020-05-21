ded09b15b6e678b62bbd60b680a8cffea40b0735
Inderjit Gill <email@indy.io>
2020-05-21 22:39:31 +0100

    random option on search page

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Net from '../lib/Net';
+import { useStateValue } from '../lib/StateProvider';
 
 export default function Search() {
-  let [state, setState] = useState({
+  const [state] = useStateValue();
+
+  let [localState, setLocalState] = useState({
     shownSearchTerm: '',
     searchResults: []
   });
@@ -11,11 +14,20 @@ export default function Search() {
   async function search(searchTerm) {
     const url = `/api/cmd/search?q=${encodeURI(searchTerm)}`;
     const searchResponse = await Net.get(url);
-    console.log(searchResponse);
+
+    const graphResponse = await Net.get('/api/cmd/graph');
+    console.log(graphResponse);
+
+    // let res1 = "";
+    // let res2 = "";
+    // state.ac.decks.forEach(ac => res1 += `{ id: ${ac.id}, label: "${ac.label}" }, `);
+    // graphResponse.results.forEach(g => res2 += `{ from: ${g.from_id}, to: ${g.to_id} }, `);
+    // console.log(res1);
+    // console.log(res2);
 
     if (searchResponse.results) {
-      setState({
-        ...state,
+      setLocalState({
+        ...localState,
         shownSearchTerm: searchTerm,
         searchResults: searchResponse.results
       });
@@ -24,6 +36,14 @@ export default function Search() {
 
   function onSearch(searchTerm){
     search(searchTerm);
+  }
+
+  function onRandom() {
+    setLocalState({
+      ...localState,
+      shownSearchTerm: '',
+      searchResults: selectRandomIdeas(state.ac.decks, 5)
+    });
   }
 
   function buildSearchResultEntry(entry, i) {
@@ -35,10 +55,17 @@ export default function Search() {
   }
 
   function buildSearchResults() {
+    let heading;
+    if (localState.shownSearchTerm.length > 0) {
+      heading = (<h2>Results for: { localState.shownSearchTerm }</h2>);
+    } else {
+      heading = (<h2>Random Ideas</h2>);
+    }
+
     return (<div>
-              <h2>Results for: { state.shownSearchTerm }</h2>,
+              { heading }
               <ol>
-                { state.searchResults.map(buildSearchResultEntry) }
+                { localState.searchResults.map(buildSearchResultEntry) }
               </ol>
             </div>);
   }
@@ -46,29 +73,56 @@ export default function Search() {
   return (
     <div>
       <h1>Search</h1>
-      <SearchBox onSearch = { onSearch }/>
-      { state.searchResults.length > 0 && buildSearchResults() }
+      <SearchBox onSearch = { onSearch } onRandom = { onRandom }/>
+      { localState.searchResults.length > 0 && buildSearchResults() }
     </div>
 
   )
 }
 
+function selectRandomIdeas(allDecks, num) {
+  let res = [];
+
+
+  let ideas = allDecks.filter(d => d.resource === 'ideas');
+  let len = ideas.length;
+
+  // check in case num > the number of ideas in civil
+  let realNum = num < len ? num : len;
+  for (let i = 0; i < realNum; i++) {
+    let index = parseInt(Math.random() * (len - i), 10);
+    res.push({
+      id: ideas[index].id,
+      name: ideas[index].label,
+      resource: 'ideas',
+    })
+    // now remove ideas[index] from the array
+    ideas.splice(index, 1);
+  };
+
+  return res;
+}
+
 function SearchBox(props) {
   let [searchTerm, setSearchTerm] = useState('');
+  let [buttonLabel, setButtonLabel] = useState('Random');
 
   const handleChangeEvent = (event) => {
     const target = event.target;
-    const name = target.name;
     const value = target.value;
 
-    if (name === 'search') {
-      setSearchTerm(value);
-    }
+    setSearchTerm(value);
+    setButtonLabel(value.length > 0 ? 'Search' : 'Random');
   };
 
   function onSubmit(event){
     event.preventDefault();
-    props.onSearch(searchTerm);
+
+    if (searchTerm.length > 0) {
+      props.onSearch(searchTerm);
+    } else {
+      props.onRandom();
+    }
   }
 
   return (
@@ -79,7 +133,7 @@ function SearchBox(props) {
              value={ searchTerm }
              onChange={ handleChangeEvent }
       />
-      <input type="submit" value="Search" />
+      <input type="submit" value={ buttonLabel } />
     </form>
   );
 }
