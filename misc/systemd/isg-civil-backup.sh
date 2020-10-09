91599c1e7300c2cfc88d97be940cebe83e844b77
Inderjit Gill <email@indy.io>
2020-10-09 11:35:36 +0100

    replaced daily backups with weekly + backing up images

--- misc/systemd/isg-civil-backup.sh
+++ misc/systemd/isg-civil-backup.sh
@@ -2,3 +2,7 @@
 
 now=`date +"%Y%m%d"`
 pg_dump civil > /home/indy/bdrive/civil/civil_${now}.psql
+
+cd /home/indy/work/civil
+tar -cf user-content-${now}.tar.gz user-content
+mv user-content-${now}.tar.gz /home/indy/bdrive/civil/.
