06ae9db26443e1f46a1ad973e6066a55ead35de3
Inderjit Gill <email@indy.io>
2020-11-02 12:00:40 +0000

    user registration started

--- www/js/components/Login.js
+++ www/js/components/Login.js
@@ -6,10 +6,16 @@ function Login({ loginCallback }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [registerPassword2, setRegisterPassword2] = useState('');
+
   const handleChangeEvent = (event) => {
     const target = event.target;
     const name = target.name;
     const value = target.value;
+
+    console.log(`${target} ${name} ${value}`);
 
     if (name === 'email') {
       setEmail(value);
@@ -17,9 +23,28 @@ function Login({ loginCallback }) {
     if (name === 'password') {
       setPassword(value);
     }
+
+    if (name === 'registeremail') {
+      setRegisterEmail(value);
+    }
+    if (name === 'registerpassword') {
+      setRegisterPassword(value);
+    }
+
+    if (name === 'registerpassword2') {
+      setRegisterPassword2(value);
+    }
   };
 
-  const handleSubmit = (event) => {
+  const handleLoginSubmit = (event) => {
+    Net.post('api/auth', { email, password }).then(user => {
+      loginCallback(user);
+    });
+
+    event.preventDefault();
+  };
+
+  const handleRegisterSubmit = (event) => {
     Net.post('api/auth', { email, password }).then(user => {
       loginCallback(user);
     });
@@ -29,7 +54,8 @@ function Login({ loginCallback }) {
 
   return html`
     <section>
-      <form onSubmit=${ handleSubmit }>
+      <h1>Login</h1>
+      <form onSubmit=${ handleLoginSubmit }>
         <label for="email">Email:</label>
         <input id="email"
                type="text"
@@ -41,6 +67,28 @@ function Login({ loginCallback }) {
                type="password"
                name="password"
                value=${ password }
+               onInput=${ handleChangeEvent } />
+        <input type="submit" value="Login"/>
+      </form>
+      <h1>Register New User</h1>
+      <form onSubmit=${ handleRegisterSubmit }>
+        <label for="registeremail">Email:</label>
+        <input id="registeremail"
+               type="text"
+               name="registeremail"
+               value=${ registerEmail }
+               onInput=${ handleChangeEvent } />
+        <label for="registerpassword">Password:</label>
+        <input id="registerpassword"
+               type="password"
+               name="registerpassword"
+               value=${ registerPassword }
+               onInput=${ handleChangeEvent } />
+        <label for="registerpassword2">Confirm Password:</label>
+        <input id="registerpassword2"
+               type="password"
+               name="registerpassword2"
+               value=${ registerPassword2 }
                onInput=${ handleChangeEvent } />
         <input type="submit" value="Login"/>
       </form>
