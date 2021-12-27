00fa485d3e972120f903a2db6ca5e951ff13fa48
Inderjit Gill <email@indy.io>
2021-12-27 14:28:51 +0000

    live search on each keystroke

--- www/js/components/Search.js
+++ www/js/components/Search.js
@@ -8,33 +8,29 @@ export default function Search() {
   const [state] = useStateValue();
 
   let [localState, setLocalState] = useState({
-    shownSearchTerm: '',
+    searchTerm: '',
     searchResults: []
   });
 
-  async function search(searchTerm) {
-    const url = `/api/cmd/search?q=${encodeURI(searchTerm)}`;
+  const handleChangeEvent = (event) => {
+    search(event.target.value);
+  };
+
+  async function search(text) {
+    const url = `/api/cmd/search?q=${encodeURI(text)}`;
     const searchResponse = await Net.get(url);
 
     if (searchResponse.results) {
       setLocalState({
         ...localState,
-        shownSearchTerm: searchTerm,
+        searchTerm: text,
         searchResults: searchResponse.results
       });
     }
   }
 
-  function onSearch(searchTerm){
-    search(searchTerm);
-  }
-
-  function onRandom() {
-    setLocalState({
-      ...localState,
-      shownSearchTerm: '',
-      searchResults: selectRandomIdeas(state.ac.decks, 5)
-    });
+  function onSubmit(e) {
+    e.preventDefault();
   }
 
   function buildSearchResultEntry(entry, i) {
@@ -47,11 +43,11 @@ export default function Search() {
 
   function buildSearchResults() {
     let heading = "";
-    if (localState.shownSearchTerm.length > 0) {
+    if (localState.searchTerm.length > 0) {
       if (localState.searchResults.length === 0) {
-        return html`<div><h2>No search results found for: ${localState.shownSearchTerm}</h2></div>`;
+        return html`<div><h2>No search results found for: ${localState.searchTerm}</h2></div>`;
       }
-      heading = html`<h2>Results for: ${ localState.shownSearchTerm }</h2>`;
+      heading = html`<h2>Results for: ${ localState.searchTerm }</h2>`;
     } else {
       if (localState.searchResults.length > 0) {
         heading = html`<h2>Random Ideas</h2>`;
@@ -71,61 +67,16 @@ export default function Search() {
   return html`
     <div>
       <h1>Full Text Search</h1>
-      <${SearchBox} onSearch=${ onSearch } onRandom=${ onRandom }/>
+      <form onSubmit=${ onSubmit }>
+        <input id="search"
+               type="text"
+               name="search"
+
+               value=${ localState.searchTerm }
+               onInput=${ handleChangeEvent }
+        />
+      </form>
       ${ buildSearchResults() }
     </div>
-`;
-}
-
-function selectRandomIdeas(allDecks, num) {
-  let res = [];
-
-  let ideas = allDecks.filter(d => d.resource === 'ideas');
-  let len = ideas.length;
-
-  // check in case num > the number of ideas in civil
-  let realNum = num < len ? num : len;
-  for (let i = 0; i < realNum; i++) {
-    let index = parseInt(Math.random() * (len - i), 10);
-    res.push(ideas[index]);
-    // now remove ideas[index] from the array
-    ideas.splice(index, 1);
-  };
-
-  return res;
-}
-
-function SearchBox(props) {
-  let [searchTerm, setSearchTerm] = useState('');
-  let [buttonLabel, setButtonLabel] = useState('Random');
-
-  const handleChangeEvent = (event) => {
-    const target = event.target;
-    const value = target.value;
-
-    setSearchTerm(value);
-    setButtonLabel(value.length > 0 ? 'Search' : 'Random');
-  };
-
-  function onSubmit(event){
-    event.preventDefault();
-
-    if (searchTerm.length > 0) {
-      props.onSearch(searchTerm);
-    } else {
-      props.onRandom();
-    }
-  }
-
-  return html`
-    <form onSubmit=${ onSubmit }>
-      <input id="search"
-             type="text"
-             name="search"
-             value=${ searchTerm }
-             onInput=${ handleChangeEvent }
-      />
-      <input type="submit" value=${ buttonLabel } />
-    </form>
 `;
 }
