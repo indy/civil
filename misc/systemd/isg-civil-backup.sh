d404108571d8c826bbab5d1216903ba28a39a302
Inderjit Gill <email@indy.io>
2022-05-02 21:08:27 +0100

    backup script compresses files

--- misc/systemd/isg-civil-backup.sh
+++ misc/systemd/isg-civil-backup.sh
@@ -2,7 +2,9 @@
 
 now=`date +"%Y%m%d"`
 pg_dump civil > /home/indy/bdrive/civil/civil_${now}.psql
+tar -Jcf /home/indy/bdrive/civil/civil_${now}.tar.xz /home/indy/bdrive/civil/civil_${now}.psql
+rm /home/indy/bdrive/civil/civil_${now}.psql
 
 cd /home/indy/work/civil
-tar -cf user-content-${now}.tar.gz user-content
-mv user-content-${now}.tar.gz /home/indy/bdrive/civil/.
+tar -Jcf user-content-${now}.tar.xz user-content
+mv user-content-${now}.tar.xz /home/indy/bdrive/civil/.
