6641357ca60ab5c139fa346ac5265e25cf1d5e70
Inderjit Gill <email@indy.io>
2022-05-15 13:32:17 +0100

    updated backup command

--- misc/systemd/isg-civil-backup.sh
+++ misc/systemd/isg-civil-backup.sh
@@ -2,8 +2,8 @@
 
 now=`date +"%Y%m%d"`
 pg_dump civil > /home/indy/bdrive/civil/civil_${now}.psql
-tar -Jcf /home/indy/bdrive/civil/civil_${now}.tar.xz /home/indy/bdrive/civil/civil_${now}.psql
-rm /home/indy/bdrive/civil/civil_${now}.psql
+
+gzip /home/indy/bdrive/civil/civil_${now}.psql
 
 cd /home/indy/work/civil
 tar -Jcf user-content-${now}.tar.xz user-content
