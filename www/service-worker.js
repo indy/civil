65835e332d37d7bb6f218bebd1c620b91c406e5e
Inderjit Gill <email@indy.io>
2022-10-02 13:04:55 +0100

    annoyance fix: Chrome Dev Tools request detected and dealt with

--- www/service-worker.js
+++ www/service-worker.js
@@ -24,6 +24,7 @@ var devMode = true;
 var CACHE_NAME = "civil-20210829";
 
 var precacheConfig = [
+    "/android-chrome-192x192.png",
     "/apple-touch-icon.png",
     "/civil.css",
     "/civil_wasm.js",
@@ -260,6 +261,40 @@ self.addEventListener("fetch", function (event) {
                         return fetch(event.request);
                     })
             );
+        } else {
+            // check for a phantom request made by Chrome's debug
+            let path = url.split("/").slice(3);
+            if (path[0] !== "api" && path[0] !== "site.webmanifest") {
+                // when opening Chromium's Developer Tools, there is a GET
+                // request made for the current page. As Civil's router
+                // re-writes the url this will result in a request to the server
+                // for a location that it doesn't understand (e.g. to /ideas
+                // when all of the requests to the server should be json calls
+                // to locations like /api/ideas).
+                //
+                // this code detects these probable fake requests and returns a fake response
+                //
+                // to see this code activated:
+                // 1. open a new tab in Chrome/Brave/Chromium etc
+                // 2. navigate to a page like localhost:3002/ideas/2800
+                // 3. open the developer tools pane
+                //
+                // opening the developer tools pane should result in one of
+                // these phantom GET requests that this code should pick up and
+                // then print some message to the console
+                //
+                console.log(`fake responding for Chrome Dev Pane's request for ${url}`);
+                event.respondWith(
+                    new Response("fake response for Chrome's Dev Tools", {
+                        status: 200,
+                        statusText: "OK",
+                        headers: { "Content-Type": "text/plain" }
+                    })
+                )
+            } else {
+                // make the actual request to the server
+            }
+
         }
     }
 });
