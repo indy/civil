147f8c464299f907d1f16e61cdd65a84c5c0e40d
Inderjit Gill <email@indy.io>
2022-10-13 09:19:21 +0100

    tidied up mod.js

--- www/lib/preact/mod.js
+++ www/lib/preact/mod.js
@@ -1,11 +1,53 @@
-import { h, createContext, createRef, render } from '/lib/preact/preact.mjs';
+import { Component, Fragment, cloneElement, createContext, createElement, createRef, h, hydrate, isValidElement, options, render, toChildArray } from '/lib/preact/preact.mjs';
 import { useState, useReducer, useEffect, useLayoutEffect, useRef, useImperativeHandle, useMemo, useCallback, useContext, useDebugValue } from '/lib/preact/hooks.mjs';
-
+import { Router, Route, Link, route } from '/lib/preact/preact-router.js';
+import { Signal, batch, computed, effect, signal, useComputed, useSignal, useSignalEffect } from '/lib/preact/signals.mjs';
 import htm from '/lib/preact/htm.mjs';
+
 const html = htm.bind(h);
 
-import { Router, Route, Link, route } from '/lib/preact/preact-router.js';
+export {
+    // preact
+    Component,
+    Fragment,
+    cloneElement,
+    createContext,
+    createElement,
+    createRef,
+    h,
+    hydrate,
+    isValidElement,
+    options,
+    render,
+    toChildArray,
 
-import { Signal, batch, computed, effect, signal, useComputed, useSignal, useSignalEffect } from '/lib/preact/signals.mjs';
+    // hooks
+    useCallback,
+    useContext,
+    useDebugValue,
+    useEffect,
+    useImperativeHandle,
+    useLayoutEffect,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
 
-export { h, html, render, createContext, createRef, useState, useReducer, useEffect, useLayoutEffect, useRef, useImperativeHandle, useMemo, useCallback, useContext, useDebugValue, Router, Route, Link, route, Signal, batch, computed, effect, signal, useComputed, useSignal, useSignalEffect };
+    // preact-router
+    Link,
+    Route,
+    Router,
+    route,
+
+    // signals
+    Signal,
+    batch,
+    computed,
+    effect,
+    signal,
+    useComputed,
+    useSignal,
+    useSignalEffect,
+
+    html
+};
