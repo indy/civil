7f56b3c5c461198f3b6f0db0a4038234a1c869fa
Inderjit Gill <email@indy.io>
2023-04-23 19:24:48 +0100

    can enlarge images

--- client/src/components/image.tsx
+++ client/src/components/image.tsx
@@ -1,12 +1,20 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 
 type ImageProps = {
     src?: string;
 };
 
 export default function Image({ src }: ImageProps) {
+    let [maximised, setMaximised] = useState(false);
+
     function onClick() {
-        console.log("clicked on an image");
+        setMaximised(!maximised);
     }
-    return <img class="deck-image" onClick={onClick} src={src} />;
+
+    let c = "deck-image";
+    if (maximised) {
+        c += " deck-image-maximised";
+    }
+    return <img class={c} onClick={onClick} src={src} />;
 }
