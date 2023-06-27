541ef78bd7bb02079323e76cd670eaa27f46da33
Inderjit Gill <email@indy.io>
2023-06-27 17:16:22 +0100

    bugfix: use-mouse-hovering-events invoked on every state change

--- client/src/components/use-mouse-hovering-events.tsx
+++ client/src/components/use-mouse-hovering-events.tsx
@@ -5,23 +5,17 @@ export default function useMouseHoveringEvents(
     onEnter: () => void,
     onLeave: () => void
 ) {
-    function mouseEnter() {
-        onEnter();
-    }
-    function mouseLeave() {
-        onLeave();
-    }
     useEffect(() => {
         if (hoveringRef && hoveringRef.current) {
             let hc = hoveringRef.current as HTMLElement;
-            hc.addEventListener("mouseenter", mouseEnter, false);
-            hc.addEventListener("mouseleave", mouseLeave, false);
+            hc.addEventListener("mouseenter", onEnter, false);
+            hc.addEventListener("mouseleave", onLeave, false);
             return () => {
-                hc.removeEventListener("mouseenter", mouseEnter);
-                hc.removeEventListener("mouseleave", mouseLeave);
+                hc.removeEventListener("mouseenter", onEnter);
+                hc.removeEventListener("mouseleave", onLeave);
             };
         }
         // added to please tsc
         return () => {};
-    }, []);
+    });
 }
