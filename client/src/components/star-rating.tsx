04b27c46f072d28c7b8236ae4bbdb4c902e01647
Inderjit Gill <email@indy.io>
2023-07-06 17:21:05 +0100

    better styling for star ratings on mobile

--- client/src/components/star-rating.tsx
+++ client/src/components/star-rating.tsx
@@ -1,6 +1,6 @@
 import { h, ComponentChildren } from "preact";
 
-import { svgRatingStar } from "components/svg-icons";
+import { svgRatingStar, svgBlank } from "components/svg-icons";
 
 import { CivLeft } from "components/civil-layout";
 
@@ -14,11 +14,19 @@ function StarRatingWithinListing({ rating }: { rating: number }) {
 
 function StarRatingPartial({ rating }: { rating: number }) {
     let ratings: Array<ComponentChildren> = [];
+
+    // pad out with blanks so that it looks correct on small display
+    // devices where the parent div will have a float left applied.
+    // (see the .star-rating-partial rule for max-width: 800px)
+    //
+    for (let i = 0; i < 5 - rating; i++) {
+        ratings.push(svgBlank());
+    }
     for (let i = 0; i < rating; i++) {
         ratings.push(svgRatingStar());
     }
 
-    return <div>{rating > 0 && ratings}</div>;
+    return <div class="star-rating-partial">{rating > 0 && ratings}</div>;
 }
 
 export { StarRatingWithinListing, StarRatingPartial };
