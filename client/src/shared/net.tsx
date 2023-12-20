38c7aaa8540d27287614e7f799c7512b1a50ec01
Inderjit Gill <email@indy.io>
2023-12-20 15:30:35 +0000

    shared/net refactored

--- client/src/shared/net.tsx
+++ client/src/shared/net.tsx
@@ -1,30 +1,30 @@
 const Net = {
     get: async function <TResp>(url: string): Promise<TResp> {
-        return go<void, TResp>("GET", url);
+        return go<TResp>(url, request("GET"));
     },
     put: async function <TData, TResp>(
         url: string,
         data: TData,
     ): Promise<TResp> {
-        return go<TData, TResp>("PUT", url, data);
+        return go<TResp>(url, addData<TData>(request("PUT"), data));
     },
     post: async function <TData, TResp>(
         url: string,
         data: TData,
     ): Promise<TResp> {
-        return go<TData, TResp>("POST", url, data);
+        return go<TResp>(url, addData<TData>(request("POST"), data));
     },
     delete: async function <TData, TResp>(
         url: string,
         data: TData,
     ): Promise<TResp> {
-        return go<TData, TResp>("DELETE", url, data);
+        return go<TResp>(url, addData<TData>(request("DELETE"), data));
     },
     getAbortable: async function <TResp>(
         url: string,
         signal: AbortSignal,
     ): Promise<TResp> {
-        return goAbortable<void, TResp>("GET", url, signal);
+        return go<TResp>(url, addSignal(request("GET"), signal));
     },
     // use getCORS when you're not allowed to set 'content-type'
     getCORS: async function <TResp>(url: string): Promise<TResp> {
@@ -32,55 +32,35 @@ const Net = {
     },
     getTimed: async function <TResp>(url: string): Promise<[TResp, number]> {
         let before = Date.now();
-        let response = await go<void, TResp>("GET", url);
+        let response = await go<TResp>(url, request("GET"));
         let after = Date.now();
 
         return [response, after - before];
     },
 };
 
-async function go<TData, TResp>(
-    method: string,
-    url: string,
-    data?: TData,
-): Promise<TResp> {
+function request(method: string): RequestInit {
     let options: RequestInit = {
         method,
         headers: {
             "content-type": "application/json",
         },
     };
-    if (data) {
-        options.body = JSON.stringify(data);
-    }
 
-    let response = await fetch(url, options);
-    if (!response.ok) {
-        throw new Error("Network response was not OK");
-    }
-
-    let ret = await response.json();
-
-    return ret as TResp;
+    return options;
 }
 
-async function goAbortable<TData, TResp>(
-    method: string,
-    url: string,
-    signal: AbortSignal,
-    data?: TData,
-): Promise<TResp> {
-    let options: RequestInit = {
-        method,
-        headers: {
-            "content-type": "application/json",
-        },
-        signal,
-    };
-    if (data) {
-        options.body = JSON.stringify(data);
-    }
+function addData<TData>(options: RequestInit, data: TData): RequestInit {
+    options.body = JSON.stringify(data);
+    return options;
+}
 
+function addSignal(options: RequestInit, signal: AbortSignal): RequestInit {
+    options.signal = signal;
+    return options;
+}
+
+async function go<TResp>(url: string, options: RequestInit): Promise<TResp> {
     let response = await fetch(url, options);
     if (!response.ok) {
         throw new Error("Network response was not OK");
